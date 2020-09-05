ed663630fcd155fb20c2a04f655a5895135bb826
Cheney <cheney256@163.com>
2020-09-05 22:51:26 +0800

    update scripts

--- fix_time_osx.sh
+++ fix_time_osx.sh
@@ -1,40 +1,45 @@
 #!/bin/sh
 #Script auto fix time on hackintosh
-#Version 1.1: Otc 11, 2015
 #OSX: 10.6 and above
 
-echo "This script required to run as root"
-
-sudo curl -o "/tmp/localtime-toggle" "https://raw.githubusercontent.com/hieplpvip/LocalTime-Toggle/master/sbin/localtime-toggle"
-sudo curl -o "/tmp/org.osx86.localtime-toggle.plist" "https://raw.githubusercontent.com/hieplpvip/LocalTime-Toggle/master/Library/LaunchDaemons/org.osx86.localtime-toggle.plist"
+DAEMON_PATH=/Library/LaunchDaemons/
+BIN_PATH=/usr/local/bin/
+TMP_PATH=/tmp/
+TIME_FIX_FILE=localtime-toggle
+TIME_DAEMON_FILE=org.osx86.localtime-toggle.plist
 
 echo "Downloading required file"
+if [ -f './sbin/'$TIME_FIX_FILE ]; then
+    cp './sbin/'$TIME_FIX_FILE $TMP_PATH
+else
+    curl -o $TMP_PATH$TIME_FIX_FILE "https://raw.githubusercontent.com/fansys/LocalTime-Toggle/master/sbin/localtime-toggle"
+fi
+if [ -f './Library/LaunchDaemons/'$TIME_DAEMON_FILE ]; then
+    cp './Library/LaunchDaemons/'$TIME_DAEMON_FILE $TMP_PATH
+else
+    curl -o $TMP_PATH$TIME_DAEMON_FILE "https://raw.githubusercontent.com/fansys/LocalTime-Toggle/master/Library/LaunchDaemons/org.osx86.localtime-toggle.plist"
+fi
 
-# exit if error
-if [ "$?" != "0" ]; then
-echo "Error: Download failure, verify network"
-exit 1
+if [ ! -d "$BIN_PATH" ] ; then
+    mkdir "$BIN_PATH" ;
 fi
 
 echo "Copy file to destination place..."
-
-sudo cp -R "/tmp/localtime-toggle" "/sbin/"
-sudo cp -R "/tmp/org.osx86.localtime-toggle.plist" "/Library/LaunchDaemons/"
-
-sudo rm /tmp/localtime-toggle
-sudo rm /tmp/org.osx86.localtime-toggle.plist
+sudo cp -R $TMP_PATH$TIME_FIX_FILE $BIN_PATH
+sudo cp -R $TMP_PATH$TIME_DAEMON_FILE $DAEMON_PATH
+sudo rm $TMP_PATH$TIME_FIX_FILE
+sudo rm $TMP_PATH$TIME_DAEMON_FILE
 
 echo "Chmod localtime-toggle..."
+sudo chmod +x $BIN_PATH$TIME_FIX_FILE
+sudo chown root $DAEMON_PATH$TIME_DAEMON_FILE
+sudo chmod 644 $DAEMON_PATH$TIME_DAEMON_FILE
 
-sudo chmod +x /sbin/localtime-toggle
-
-echo "Chmod org.osx86.localtime-toggle.plist..."
-
-sudo chown root /Library/LaunchDaemons/org.osx86.localtime-toggle.plist
-sudo chmod 644 /Library/LaunchDaemons/org.osx86.localtime-toggle.plist
-
-echo "Load LaunchDaemons..."
-
-sudo launchctl load -w /Library/LaunchDaemons/org.osx86.localtime-toggle.plist
+if sudo launchctl list | grep --quiet localtime-toggle; then
+    echo "Stopping existing localtime-toggle daemon."
+    sudo launchctl unload $DAEMON_PATH$TIME_DAEMON_FILE
+fi
+echo "Load Localtime-toggle..."
+sudo launchctl load -w $DAEMON_PATH$TIME_DAEMON_FILE
 
 echo "Done!"
---
+++ uninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#Script auto fix time on hackintosh
+#OSX: 10.6 and above
+
+DAEMON_PATH=/Library/LaunchDaemons/
+BIN_PATH=/usr/local/bin/
+TIME_FIX_FILE=localtime-toggle
+TIME_DAEMON_FILE=org.osx86.localtime-toggle.plist
+
+if sudo launchctl list | grep --quiet localtime-toggle; then
+    echo "Stopping existing localtime-toggle daemon."
+    sudo launchctl unload $DAEMON_PATH$TIME_DAEMON_FILE
+fi
+
+echo "Delete files..."
+sudo rm $BIN_PATH$TIME_FIX_FILE
+sudo rm $DAEMON_PATH$TIME_DAEMON_FILE
+
+echo "Done!"
